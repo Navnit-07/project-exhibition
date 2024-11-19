97ce0d34f33b6f0082c4e2c1fb460b33d9ad03b1
Navnit <178172653+Navnit-07@users.noreply.github.com>
2024-11-19 14:12:21 +0530

    add show-hide functionality of password

--- Components/inputField/Input.js
+++ Components/inputField/Input.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Input.module.css'
 
-export default function Input({ type, placeholder, classNameInp, classNameImg, src }) {
-  return (
-    <div className={styles['section-div1']}>
-      <input type={type} placeholder={placeholder} className={`${styles['search-input']} ${classNameInp || ''}`} />
-      {src?(<img src={src} className={`${styles['section-img']} ${classNameImg || ''}`} />):(null)}
-    </div>
-  )
+export default function Input({ type, 
+                                placeholder, 
+                                classNameInp, 
+                                classNameImg, 
+                                src, 
+                                srcDisplay, 
+                                srcHide, 
+                                showHide
+}) {
+
+    const [displayPass, setDisplayPass] = useState(false)
+
+    const toggleDisplay = () => {
+      setDisplayPass((prev) => !prev)
+    }
+
+    const Type = !displayPass && type === 'password'? 'password' : 'text'
+
+    const source = (displayPass ? (srcHide) : (srcDisplay))
+
+    return (
+      <div className={styles['section-div1']}>
+
+        <input 
+              type={Type} 
+              placeholder={placeholder} 
+              className={`${styles['search-input']} ${classNameInp || ''}`} 
+        />
+
+        {src ? (
+
+          <img 
+              src={(Type === 'password' ? (source) : (showHide? source : src))} 
+              className={`${styles['section-img']} ${classNameImg || ''}`}
+              onClick={showHide ? (toggleDisplay) : (null)} 
+          />)
+
+        : (null)}
+      </div>
+    )
 }
--- Components/login/Login.js
+++ Components/login/Login.js
@@ -3,12 +3,14 @@ import styles from './Login.module.css'
 import Input from '../inputField/Input'
 import Button from '../button/Button'
 import PersonImg from '../../Assets/person.png'
+import ShowImg from '../../Assets/show-pass.png'
+import HideImg from '../../Assets/hide-pass.png'
 export default function Login() {
   return (
     <>
         <div className={styles['login-container']}>
             <Input type='text' placeholder='Enter username/email' src={PersonImg}/>
-            <Input type='password' placeholder='Enter password' src='' />
+            <Input type='password' placeholder='Enter password' srcDisplay={ShowImg} srcHide={HideImg} showHide={true} src={true}/>
             <Button text='Log In'/>
         </div>
     </>
